f3766e724c5460c69f8f3b33ac447dd51b23d8e4
sadmanador <gazitasnuva.nsu@gmail.com>
2023-01-10 21:38:49 +0600

    Module 61-2 done!

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-res.send('Hello world')
+    res.send('News API is running')
 });
 
 app.get('/new-categories', (req, res) => {
-res.send(categories)
+    res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else{
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+});
+
+app.get('/news', (req, res) => {
+    res.send(news)
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 });
 
 app.listen(port, () => {
-console.log(`listening port 5000`);
+    console.log(`listening port 5000`);
 });
